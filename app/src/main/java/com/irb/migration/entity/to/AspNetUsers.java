f10d8e581faeae524c31a72fbd3ddaebf6592602
diegoarpar <diegoarpar@gmail.com>
2024-07-16 16:37:58 -0500

    first users version (#1)
    Co-authored-by: Diego Padilla <diego.padilla@mercadolibre.com.co>

--- app/src/main/java/com/irb/migration/entity/to/AspNetUsers.java
+++ app/src/main/java/com/irb/migration/entity/to/AspNetUsers.java
@@ -1,4 +1,58 @@
 package com.irb.migration.entity.to;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "AspNetUsers")
 public class AspNetUsers {
+
+    @jakarta.persistence.Id
+    @Column(name = "Id", columnDefinition = "NUMERIC(19,0)")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    public Integer Id;
+
+    @Column(name = "UserName")
+    public String UserName;
+
+    @Column(name = "NormalizedUserName")
+    public String NormalizedUserName;
+
+    @Column(name = "Email")
+    public String Email;
+
+    @Column(name = "NormalizedEmail")
+    public String NormalizedEmail;
+
+    @Column(name = "EmailConfirmed")
+    public Integer EmailConfirmed;
+
+    @Column(name = "PasswordHash")
+    public String PasswordHash;
+
+    @Column(name = "SecurityStamp")
+    public String SecurityStamp;
+
+    @Column(name = "ConcurrencyStamp")
+    public String ConcurrencyStamp;
+
+    @Column(name = "PhoneNumber")
+    public String PhoneNumber;
+
+    @Column(name = "PhoneNumberConfirmed")
+    public String PhoneNumberConfirmed;
+
+    @Column(name = "TwoFactorEnabled")
+    public String TwoFactorEnabled;
+
+    @Column(name = "LockoutEnd", columnDefinition = "DATE")
+    public Date LockoutEnd;
+
+    @Column(name = "LockoutEnabled")
+    public Integer LockoutEnabled;
+
+    @Column(name = "")
+    public Integer AccessFailedCount;
+
 }
--- app/src/main/java/com/irb/migration/service/transforms/UserProfilesTransformation.java
+++ app/src/main/java/com/irb/migration/service/transforms/UserProfilesTransformation.java
@@ -1,7 +1,10 @@
 package com.irb.migration.service.transforms;
 
 import com.irb.migration.entity.from.UserDetails;
+import com.irb.migration.entity.to.AspNetUsers;
+import com.irb.migration.entity.to.Universities;
 import com.irb.migration.entity.to.UserProfiles;
+import java.text.SimpleDateFormat;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,11 +12,42 @@ import java.util.stream.Collectors;
 public class UserProfilesTransformation implements ETLTransformation<UserProfiles, UserDetails> {
 
     public List<UserProfiles> TransformData(List<UserDetails> sourceData) {
+        Universities universities = new Universities();
+        universities.Name = "Gannon University";
         return sourceData.stream().map(source -> {
-            UserProfiles dest = new UserProfiles();
-            dest.FirstName  = source.firstname;
-            dest.LastName = source.last_name;
-            return dest;
+            AspNetUsers aspNetUsers = new AspNetUsers();
+            aspNetUsers.UserName = source.gu_email;
+            aspNetUsers.NormalizedEmail = source.gu_email.toUpperCase();
+            aspNetUsers.Email = source.gu_email;
+            aspNetUsers.EmailConfirmed = "1".equalsIgnoreCase(source.admin_approval)? 1: 0 ;
+            aspNetUsers.UserName = source.gu_email;
+            aspNetUsers.PhoneNumber = source.contact_no;
+            aspNetUsers.PhoneNumberConfirmed = source.contact_no;
+            aspNetUsers.TwoFactorEnabled = "0";
+            aspNetUsers.LockoutEnabled = 1;
+
+            UserProfiles userProfileUser = new UserProfiles();
+            userProfileUser.UserId = aspNetUsers;
+            userProfileUser.FirstName  = source.firstname;
+            userProfileUser.LastName = source.last_name;
+            userProfileUser.MiddleName = source.middle_name;
+            userProfileUser.Title = source.title;
+            userProfileUser.Picture = source.img;
+            userProfileUser.CreatedDate = source.reg_date;
+            try {
+                userProfileUser.LastLogin = new SimpleDateFormat("yyyyMMddmmss").parse(source.last_visit);
+            } catch (Exception e) {
+
+            }
+            userProfileUser.University = universities;
+            userProfileUser.Campus = source.campus;
+            userProfileUser.Department = source.department;
+            userProfileUser.ResearchArea = source.research_area;
+            userProfileUser.Gender = source.gender;
+            userProfileUser.Role = source.user_type;
+
+
+            return userProfileUser;
         }).collect(Collectors.toList());
     }
 }
